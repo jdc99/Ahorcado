857b8bf362ec929c4107ec7b02b9781b4a4c3803
ifram <iframeestudios@gmail.com>
2025-12-15 16:11:44 +0100

    Creada estructura de clases y paquetes

---
+++ src/pkg/CanvasAhorcado.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package pkg;
+
+import java.awt.Canvas;
+
+/**
+ *
+ * @author ifram
+ */
+public class CanvasAhorcado extends Canvas {
+    
+}
