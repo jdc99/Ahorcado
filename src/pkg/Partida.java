f9b80b98311e40c0799f707fde89a7eb4d0fc859
ifram <iframeestudios@gmail.com>
2025-12-15 17:10:11 +0100

    Implementada clase Partida a excepcion del canvas

--- src/pkg/Partida.java
+++ src/pkg/Partida.java
@@ -9,5 +9,42 @@ package pkg;
  * @author ifram
  */
 public class Partida {
-    
+
+    private static final int MAXIMO_INTENTOS = 6;
+
+    private final String palabra;
+    private final CanvasAhorcado canvas;
+
+    private int numIntentos;
+
+    public Partida(String palabra, Jugador adivinador, Jugador retador, CanvasAhorcado canvas) {
+        this.palabra = palabra;
+        this.canvas = canvas;
+
+        numIntentos = MAXIMO_INTENTOS;
+    }
+
+    public char[] comprobarPalabra(char[] intento) {
+        numIntentos--;
+
+        var palabraCaracterizada = palabra.toCharArray();
+        var numCaracteres = palabraCaracterizada.length;
+
+        char[] respuesta = new char[numCaracteres];
+
+        for (char item : intento) {
+            for (int j = 0; j < numCaracteres; j++) {
+                respuesta[j] = ' ';
+                if (item == palabraCaracterizada[j]) {
+                    respuesta[j] = item;
+                }
+            }
+
+        }
+        return respuesta;
+    }
+
+    public int getNumIntentos() {
+        return numIntentos;
+    }
 }
